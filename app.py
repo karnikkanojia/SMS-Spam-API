d1d56abeab0bd39a79b97959641416b874605ded
Karnik Kanojia <72961346+karnikkanojia@users.noreply.github.com>
2022-01-20 13:46:07 +0530

    Added error handling for bad requests or empty strings

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from dotenv import load_dotenv
 import os
 from utils.score import get_scores
+from errors.invalid import InvalidAPIUsage
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 app = Flask(__name__)
@@ -11,9 +12,19 @@ config = load_dotenv()
 def invalid(error):
     return render_template('404.html')
 
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return jsonify(message="Method Not Allowed"), 405
+
+@app.errorhandler(InvalidAPIUsage)
+def invalid_usage(error):
+    return jsonify(error.to_dict()), 400
+
 @app.route('/api')
 def score():
     message = request.form.get('message')
+    if message is None or message == '':
+        raise InvalidAPIUsage("No user id provided!")
     score = get_scores(message)
     return {
         'score': score
